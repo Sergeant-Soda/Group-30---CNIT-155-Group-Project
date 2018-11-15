d77879288d03955e2c77ad531f50388c47bcfd51
Sergeant-Soda <michaelgeorgekelly@gmail.com>
2018-11-15 15:04:27 -0500

    Add files via upload

---
+++ Group 30 - CNIT 155 Group Project/Group 30 - CNIT 155 Group Project/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Group_30___CNIT_155_Group_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPPhone = new System.Windows.Forms.TextBox();
+            this.txtWPhone = new System.Windows.Forms.TextBox();
+            this.txtEMail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radOther = new System.Windows.Forms.RadioButton();
+            this.radWork = new System.Windows.Forms.RadioButton();
+            this.radFriend = new System.Windows.Forms.RadioButton();
+            this.radFamily = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(258, 9);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(237, 264);
+            this.lstOutput.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(177, 250);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(177, 191);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 2;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(177, 220);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(177, 162);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 4;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(103, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Work Phone:";
+            // 
+            // txtPPhone
+            // 
+            this.txtPPhone.Location = new System.Drawing.Point(103, 74);
+            this.txtPPhone.Name = "txtPPhone";
+            this.txtPPhone.Size = new System.Drawing.Size(149, 20);
+            this.txtPPhone.TabIndex = 10;
+            // 
+            // txtWPhone
+            // 
+            this.txtWPhone.Location = new System.Drawing.Point(103, 100);
+            this.txtWPhone.Name = "txtWPhone";
+            this.txtWPhone.Size = new System.Drawing.Size(149, 20);
+            this.txtWPhone.TabIndex = 11;
+            // 
+            // txtEMail
+            // 
+            this.txtEMail.Location = new System.Drawing.Point(103, 126);
+            this.txtEMail.Name = "txtEMail";
+            this.txtEMail.Size = new System.Drawing.Size(149, 20);
+            this.txtEMail.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Personal Phone:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "E-Mail:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radOther);
+            this.groupBox1.Controls.Add(this.radWork);
+            this.groupBox1.Controls.Add(this.radFriend);
+            this.groupBox1.Controls.Add(this.radFamily);
+            this.groupBox1.Location = new System.Drawing.Point(15, 162);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(156, 111);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Affiliation";
+            // 
+            // radOther
+            // 
+            this.radOther.AutoSize = true;
+            this.radOther.Location = new System.Drawing.Point(6, 88);
+            this.radOther.Name = "radOther";
+            this.radOther.Size = new System.Drawing.Size(51, 17);
+            this.radOther.TabIndex = 3;
+            this.radOther.TabStop = true;
+            this.radOther.Text = "Other";
+            this.radOther.UseVisualStyleBackColor = true;
+            // 
+            // radWork
+            // 
+            this.radWork.AutoSize = true;
+            this.radWork.Location = new System.Drawing.Point(6, 65);
+            this.radWork.Name = "radWork";
+            this.radWork.Size = new System.Drawing.Size(51, 17);
+            this.radWork.TabIndex = 2;
+            this.radWork.TabStop = true;
+            this.radWork.Text = "Work";
+            this.radWork.UseVisualStyleBackColor = true;
+            // 
+            // radFriend
+            // 
+            this.radFriend.AutoSize = true;
+            this.radFriend.Location = new System.Drawing.Point(6, 42);
+            this.radFriend.Name = "radFriend";
+            this.radFriend.Size = new System.Drawing.Size(54, 17);
+            this.radFriend.TabIndex = 1;
+            this.radFriend.TabStop = true;
+            this.radFriend.Text = "Friend";
+            this.radFriend.UseVisualStyleBackColor = true;
+            // 
+            // radFamily
+            // 
+            this.radFamily.AutoSize = true;
+            this.radFamily.Location = new System.Drawing.Point(6, 19);
+            this.radFamily.Name = "radFamily";
+            this.radFamily.Size = new System.Drawing.Size(54, 17);
+            this.radFamily.TabIndex = 0;
+            this.radFamily.TabStop = true;
+            this.radFamily.Text = "Family";
+            this.radFamily.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 31);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Address Book";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 298);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEMail);
+            this.Controls.Add(this.txtWPhone);
+            this.Controls.Add(this.txtPPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lstOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPPhone;
+        private System.Windows.Forms.TextBox txtWPhone;
+        private System.Windows.Forms.TextBox txtEMail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radOther;
+        private System.Windows.Forms.RadioButton radWork;
+        private System.Windows.Forms.RadioButton radFriend;
+        private System.Windows.Forms.RadioButton radFamily;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Group 30 - CNIT 155 Group Project/Group 30 - CNIT 155 Group Project/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_30___CNIT_155_Group_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int cSize = 100;
+        private string[] mName = new string[cSize];
+        private string[] mPPhone = new string[cSize];
+        private string[] mWPhone = new string[cSize];
+        private string[] mEMail = new string[cSize];
+        private string[] mAffiliation = new string[cSize];
+        private int mIndex = 0;
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
